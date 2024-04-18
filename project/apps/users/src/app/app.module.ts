7ab23c73768fc0ee283ae58839270ea78c192649
Дмитрий <shcheklein.dev@gmail.com>
2024-04-18 23:38:55 +0300

    3.9. Подключит UsersConfigModule в users.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from '@project/blog-user';
 import { AuthenticationModule } from '@project/authentication';
+import { UsersConfigModule } from '@project/config';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule],
+  imports: [BlogUserModule, AuthenticationModule, UsersConfigModule],
   controllers: [],
   providers: [],
 })
