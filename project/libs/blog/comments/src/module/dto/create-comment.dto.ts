871ccda14d178e6a8ffd6aa6c263d4e4c8a9e7d4
Dmitry <shcheklein.dev@gmail.com>
2024-04-23 19:50:25 +0300

    5.1. Добавит базовую валидацию для сервиса users, blog.

--- project/libs/blog/comments/src/module/dto/create-comment.dto.ts
+++ project/libs/blog/comments/src/module/dto/create-comment.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Comment } from '@project/shared/core';
-// import { IsString, MinLength, MaxLength } from 'class-validator';
-// import { CommentValidator } from '@project/validation';
+import { IsString, MinLength, MaxLength } from 'class-validator';
+import { CommentValidator } from '@project/validation';
 
 export class CreateCommentDto
   implements Omit<Comment, 'createdAt' | 'updatedAt'>
@@ -10,14 +10,14 @@ export class CreateCommentDto
     description: 'Comment message',
     example: 'Lorem ipsum',
   })
-  // @IsString()
-  // @MinLength(CommentValidator.message.Min)
-  // @MaxLength(CommentValidator.message.Max)
+  @IsString()
+  @MinLength(CommentValidator.message.Min)
+  @MaxLength(CommentValidator.message.Max)
   public message!: string;
 
   @ApiProperty({
     description: 'Post id',
-    example: '239acf4a-8fac-4500-8b3e-120f2b802ee6',
+    example: '52b7a93fe29bcc5e9410a607',
   })
   public postId!: string;
 
--- project/libs/users/authentication/src/dto/login-user.dto.ts
+++ project/libs/users/authentication/src/dto/login-user.dto.ts
@@ -1,4 +1,23 @@
+import { AUTH_USER_EMAIL_NOT_VALID } from '../authentication-module/authentication.constant';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
+import { User } from '@project/validation';
+
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.ru',
+  })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email!: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456',
+    minLength: User.password.Min,
+    maxLength: User.password.Max,
+  })
+  @MinLength(User.password.Min)
+  @MaxLength(User.password.Max)
   public password!: string;
 }
