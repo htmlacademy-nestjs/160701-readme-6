83f0a8f4496971c15e0152c4a62512df07b875d1
Дмитрий <shcheklein.dev@gmail.com>
2024-05-09 12:42:45 +0300

    fix: Handlebars template

--- project/libs/shared/config/src/configurations/mail/get-mailer-options.ts
+++ project/libs/shared/config/src/configurations/mail/get-mailer-options.ts
@@ -25,7 +25,7 @@ export function getMailerAsyncOptions(
           dir: resolve(__dirname, 'assets'),
           adapter: new HandlebarsAdapter(),
           options: {
-            strict: true,
+            strict: false,
           },
         },
       };
