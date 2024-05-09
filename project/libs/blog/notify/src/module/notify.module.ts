f60908cb1325d00e13cd78e627666d7b5edfa97d
Дмитрий <shcheklein.dev@gmail.com>
2024-05-09 12:25:37 +0300

    feat: add blog-notify module

---
+++ project/libs/blog/notify/src/module/notify.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { NotifyService } from './notify.service';
+import { getRabbitMQOptions } from '@project/config';
+
+@Module({
+  imports: [RabbitMQModule.forRootAsync(RabbitMQModule, getRabbitMQOptions())],
+  providers: [NotifyService],
+  exports: [NotifyService],
+})
+export class NotifyModule {}
