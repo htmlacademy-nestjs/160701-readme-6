838ef877939a4cd0e8374ceec61e5e650ea828b3
Дмитрий <shcheklein.dev@gmail.com>
2024-05-09 12:31:29 +0300

    feat: add new posts send from cron

--- project/libs/blog/posts/src/module/post.module.ts
+++ project/libs/blog/posts/src/module/post.module.ts
@@ -6,9 +6,16 @@ import { PostRepository } from './repositories/post.repository';
 import { PostFactory } from './post.factory';
 import { PostsContentModule } from './post-content/post-content.module';
 import { TagModule } from '@project/blog-tags';
-
+import { ScheduleModule } from '@nestjs/schedule';
+import { NotifyModule } from '@project/blog-notify';
 @Module({
-  imports: [PrismaClientModule, PostsContentModule, TagModule],
+  imports: [
+    PrismaClientModule,
+    PostsContentModule,
+    TagModule,
+    ScheduleModule.forRoot(),
+    NotifyModule,
+  ],
   controllers: [PostController],
   providers: [PostService, PostRepository, PostFactory],
   exports: [PostService],
---
+++ project/libs/shared/core/src/lib/types/cronTime.enum.ts
@@ -0,0 +1,7 @@
+export enum CronTime {
+  EVERY_5_SECONDS = '*/5 * * * * *',
+  EVERY_30_SECONDS = '*/30 * * * * *',
+  EVERY_MINUTE = '0 * * * * *',
+  EVERY_HOUR = '0 0 * * * *',
+  EVERY_DAY = '0 0 * * *', 
+}
