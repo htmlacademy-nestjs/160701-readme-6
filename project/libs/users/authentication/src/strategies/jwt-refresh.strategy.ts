4a73f2026a10bcb4a16bcc86a9e31aaa74585066
Дмитрий <shcheklein.dev@gmail.com>
2024-05-13 14:57:23 +0300

    7.14. Обновит стратегию JwtRefreshStrategy

--- project/libs/users/authentication/src/strategies/jwt-refresh.strategy.ts
+++ project/libs/users/authentication/src/strategies/jwt-refresh.strategy.ts
@@ -4,9 +4,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigType } from '@nestjs/config';
 
 import { jwtConfig } from '@project/config';
-import { TokenPayload } from '@project/shared/core';
+import { RefreshTokenPayload } from '@project/shared/core';
 
 import { AuthenticationService } from '../authentication-module/authentication.service';
+import { RefreshTokenService } from '../refresh-token-module/refresh-token.service';
+import { TokenNotExistsException } from '../exceptions/token-not-exists.exception';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
@@ -16,7 +18,8 @@ export class JwtRefreshStrategy extends PassportStrategy(
   constructor(
     @Inject(jwtConfig.KEY)
     private readonly jwtOptions: ConfigType<typeof jwtConfig>,
-    private readonly authService: AuthenticationService
+    private readonly authService: AuthenticationService,
+    private readonly refreshTokenService: RefreshTokenService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -24,7 +27,18 @@ export class JwtRefreshStrategy extends PassportStrategy(
     });
   }
 
-  public async validate(payload: TokenPayload) {
+  public async validate(payload: RefreshTokenPayload) {
+    const { tokenId } = payload;
+
+    const isExists = await this.refreshTokenService.isExists(tokenId);
+
+    if (!isExists) {
+      throw new TokenNotExistsException(tokenId);
+    }
+
+    await this.refreshTokenService.deleteRefreshSession(tokenId);
+    await this.refreshTokenService.deleteExpiredRefreshTokens();
+    
     return this.authService.getUserByEmail(payload.email);
   }
 }
