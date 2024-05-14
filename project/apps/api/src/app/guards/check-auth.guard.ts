ceabddd8c7176add2e976133bad17aded86bc2a2
Дмитрий <shcheklein.dev@gmail.com>
2024-05-14 14:13:27 +0300

    7.27. Добавит CheckAuthGuard для сервиса api.

---
+++ project/apps/api/src/app/guards/check-auth.guard.ts
@@ -0,0 +1,25 @@
+import { HttpService } from '@nestjs/axios';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ApplicationServiceURL } from '../app.config';
+
+@Injectable()
+export class CheckAuthGuard implements CanActivate {
+  constructor(private readonly httpService: HttpService) {}
+
+  public async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const { data } = await this.httpService.axiosRef.post(
+      `${ApplicationServiceURL.Users}/check`,
+      {},
+      {
+        headers: {
+          Authorization: request.headers['authorization'],
+        },
+      }
+    );
+
+    request['user'] = data;
+
+    return true;
+  }
+}
