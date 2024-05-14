6bac51d0395ecdf0164c59a8c034a301e3961cdb
Дмитрий <shcheklein.dev@gmail.com>
2024-05-14 11:53:33 +0300

    7.21. Подключит HttpModule в app.module.ts.

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ApiConfigModule } from '@project/config';
+import {
+  HTTP_CLIENT_MAX_REDIRECTS,
+  HTTP_CLIENT_TIMEOUT
+} from './app.config'
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ApiConfigModule],
+  imports: [
+    ApiConfigModule,
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
